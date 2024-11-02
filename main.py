988e9c8f872d2d5d3e7cc7ba5800c6336c380219
SergeyAnuf <sergeysyava38@gmail.com>
2024-11-02 21:30:22 +0300

    Добавлена автоматическое копирование в буфер обмена

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from tkinter import ttk
 import requests
 from bottle import response
 from tkinter import messagebox as mb
+import pyperclip
 
 def upload():
     try:
@@ -16,6 +17,8 @@ def upload():
                 link = response.json()["link"]
                 entry.delete(0, END)
                 entry.insert(0, link)
+                pyperclip.copy(link)
+                mb.showinfo("Ссылка скопирована", f"Ссылка {link} успешно скопирована")
     except Exception as e:
         mb.showerror("Ошибка", f"Произошла ошибка {e}")
 
